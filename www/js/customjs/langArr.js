d0cf0d541dddcdc27b4d2caf427c83b63adb4432
vcshing <vcshing2005@hotmail.com>
2017-12-29 00:08:57 +0800

    asd

---
+++ www/js/customjs/langArr.js
@@ -0,0 +1,55 @@
+var lang
+var appConfigArr = []
+var defaultLangArr = []
+var menuArr= []
+var menuSiteArr= []
+var loopLangArr = []
+var imageName = []
+appConfigArr={
+  "appName" :	"Best Choice",
+  "appDescription"	:	"Best Choice",
+  "androidAppLink"	:"https://play.google.com/store/apps/details?id=com.skyexplorer.goodprice",
+  "androidAdmobBannerID"	:"ca-app-pub-3715336230214756/1437729084",
+  "androidAdmobInterstitialID"	:"ca-app-pub-3715336230214756/8549932345"
+}
+
+
+imageName.push("./image/fish.png")
+imageName.push("./image/shrimp.png")
+imageName.push("./image/crab.png")
+imageName.push("./image/money.png")
+imageName.push("./image/wo.png")
+imageName.push("./image/chicken.png")
+
+defaultLangArr.push("Other Apps");
+defaultLangArr.push("Share Apps");
+defaultLangArr.push("Default Language");
+defaultLangArr.push("計算");
+
+defaultLangArr.push("產品A");
+defaultLangArr.push("產品B");
+defaultLangArr.push("金額");
+defaultLangArr.push("金額");
+defaultLangArr.push("金額");
+defaultLangArr.push("數量");
+defaultLangArr.push("數量");
+defaultLangArr.push("數量");
+defaultLangArr.push("每件重量");
+defaultLangArr.push("每件重量");
+defaultLangArr.push("每件重量");
+defaultLangArr.push("建議");
+defaultLangArr.push("最便宜");
+
+defaultLangArr.push("產品A");
+defaultLangArr.push("每一元");
+defaultLangArr.push("每一單位");
+
+defaultLangArr.push("產品B");
+defaultLangArr.push("每一元");
+defaultLangArr.push("每一單位");
+
+menuSiteArr.push("http://www.metro.taipei/");
+menuSiteArr.push("https://www.krtco.com.tw/");
+menuSiteArr.push("https://www.tymetro.com.tw");
+menuSiteArr.push("http://www.thsrc.com.tw");
+menuSiteArr.push("http://www.railway.gov.tw/tw/");
